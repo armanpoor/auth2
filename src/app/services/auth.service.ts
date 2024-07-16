869344a143612c64c0429cd43a4749ff8af33777
armanpoor <hossein.armanpoor@gmail.com>
2024-07-16 04:51:57 +0330

    2

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,28 +1,73 @@
 import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient, Session } from '@supabase/supabase-js';
+import {
+  createClient,
+  SupabaseClient,
+  Session,
+  AuthSession,
+  AuthUser,
+} from '@supabase/supabase-js';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
+
+interface AuthResponse {
+  data: {
+    user: AuthUser | null;
+    session: AuthSession | null;
+  };
+  error: any;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private currentUser: AuthUser | null = null;
   private supabase: SupabaseClient;
   private session: Session | null = null;
+  // currentUser: any;
 
   constructor(private router: Router) {
     this.supabase = createClient(
       environment.supabaseUrl,
       environment.supabaseKey
     );
-
-    this.supabase.auth.getSession().then(({ data: { session } }) => {
-      this.session = session;
+    this.supabase.auth
+      .getSession()
+      .then((res) => {
+        this.session = res.data?.session || null;
+      })
+      .catch((error) => {
+        console.error('Error getting session:', error);
+      });
+    this.supabase.auth.onAuthStateChange((_event, session: Session | null) => {
+      if (session) {
+        this.session = session;
+      } else {
+        this.session = null;
+        console.error('Error getting session:', Error);
+      }
     });
+  }
+  async registerUser(
+    email: string,
+    password: string
+  ): Promise<AuthUser | null> {
+    try {
+      const { data, error }: AuthResponse = await this.supabase.auth.signUp({
+        email,
+        password,
+      });
 
-    this.supabase.auth.onAuthStateChange((_event, session) => {
-      this.session = session;
-    });
+      if (error) {
+        throw error;
+      }
+
+      this.currentUser = data.user; // Store current user in service
+
+      return data.user; // Return the user object upon successful registration
+    } catch (error) {
+      throw error;
+    }
   }
 
   async login(email: string, password: string): Promise<void> {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://your-supabase-url.supabase.co',
-  supabaseKey: 'your-supabase-key',
+  supabaseUrl: 'https://utarctmkazdkvwchzvdq.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0YXJjdG1rYXpka3Z3Y2h6dmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwNzM2MzYsImV4cCI6MjAzNjY0OTYzNn0.71hG6PIjnHl9RMEZvPhVS30Tt3yMXQZiu5odEQplL6A',
 };
